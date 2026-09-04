ed30bf405332a3a864a781bbe8b64e3473d94957
yuyu <66120800+yuyu107@users.noreply.github.com>
2026-09-05 06:33:53 +0800

    feat: restore follows newest patched FeverGames version

--- 03_Restore_Official_Original.cmd
+++ 03_Restore_Official_Original.cmd
@@ -1,6 +1,6 @@
 @echo off
 setlocal
 cd /d "%~dp0"
-powershell.exe -NoProfile -ExecutionPolicy Bypass -File "%~dp0Restore_Official_Original_v1.2.ps1"
+powershell.exe -NoProfile -ExecutionPolicy Bypass -File "%~dp0Restore_Latest_FeverGames_v1.3.ps1"
 echo.
 pause
