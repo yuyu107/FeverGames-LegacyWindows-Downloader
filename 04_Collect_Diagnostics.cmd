839a4c0407e8bfb1649130d1187491156b095605
yuyu <66120800+yuyu107@users.noreply.github.com>
2026-09-05 09:08:55 +0800

    Use v1.3 diagnostics collector

--- 04_Collect_Diagnostics.cmd
+++ 04_Collect_Diagnostics.cmd
@@ -1,6 +1,6 @@
 @echo off
 setlocal
 cd /d "%~dp0"
-powershell.exe -NoProfile -ExecutionPolicy Bypass -File "%~dp0Collect_Diagnostics_v1.2.ps1"
+powershell.exe -NoProfile -ExecutionPolicy Bypass -File "%~dp0Collect_Diagnostics_v1.3.ps1"
 echo.
 pause
